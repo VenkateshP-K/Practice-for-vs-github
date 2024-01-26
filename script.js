b0d7d9966b8894374d8aef9c5d00b5d0b90529a4
VenkateshP-K <venkateshpk06@gmail.com>
2024-01-26 12:31:24 +0530

    complete XML Http

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //create a XML-HTTP Object
 var request =  new XMLHttpRequest();
 //open a request
-request.open("GET","https://restcountries.com/v3.1/all")
+request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload = function(){
+    {
+    var res = JSON.parse(request.response);
+    console.log(res);
+}
+for(var i =0;i<res.length;i++){
+    if(res[i].capital){
+        console.log(res[i].capital[0]+" "+res[i].area)
+    }
+    else{
+        console.log("Invalid capital"+res[i].area);
+    }
+}
+}
