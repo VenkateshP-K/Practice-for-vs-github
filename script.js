c2ede0187d2d0c26ce1156d6ae95d11cbc5098b8
Venkatesh <venkateshpk06@gmail.com>
2024-02-05 12:10:46 +0530

    update new api

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 //create a XML-HTTP Object
 var request =  new XMLHttpRequest();
 //open a request
-request.open("GET","https://restcountries.com/v3.1/all");
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
 request.send();
 request.onload = function(){
-    {
     var res = JSON.parse(request.response);
     console.log(res);
-}
-for(var i =0;i<res.length;i++){
-    if(res[i].capital){
-        console.log(res[i].capital[0]+" "+res[i].area)
-    }
-    else{
-        console.log("Invalid capital"+res[i].area);
-    }
-}
+    var result_asia = res.filter((ele) => ele.region == "Asia");
+    console.log(result_asia)
+    var country_names = result_asia.map((ele) => ele.name)
+    console.log(country_names)
+
+    var pop = res.filter((ele2) => ele2.population<200000)
+    console.log(pop)
+    var pop_cap = pop.map((ele2)=>ele2.capital)
+    console.log(pop_cap)
 }
