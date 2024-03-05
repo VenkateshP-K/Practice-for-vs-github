a13e42d3c4b6becec1ba5ac6d70f25ee4b597aab
Venkatesh <venkateshpk06@gmail.com>
2024-03-05 10:23:02 +0530

    commit

---
+++ Today/script.js
@@ -0,0 +1,19 @@
+var div = document.createElement('div');
+div.innerHTML = `<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+  </tbody>
+</table>`
+document.body.append(div);
